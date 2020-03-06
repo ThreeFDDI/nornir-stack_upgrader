a31ef4d440d9e0ae8be53f0cc84258d622368440
JT <foosyou@gmail.com>
2020-03-06 09:24:39 -0600

    more tshoot

--- stack_upgrader_http.py
+++ stack_upgrader_http.py
@@ -127,8 +127,8 @@ def upgrade_3750(task):
 def upgrade_3650(task):
     print(f"{task.host}: Upgraging Catalyst 3650 software.")
     upgrade_img = task.host['upgrade_img']
-    task.host['current_version'].startswith("16"):
-    print("16.x")
+#    task.host['current_version'].startswith("16"):
+#    print("16.x")
     cmd = f"request platform software package install switch all file http://10.165.13.125:8000/{upgrade_img} new auto-copy"
 #    else:
 #        print("NOT 16.x")
