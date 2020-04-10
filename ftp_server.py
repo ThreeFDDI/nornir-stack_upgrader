dba18571b0cad2cbde2c737799d1457efb790ab0
JT <foosyou@gmail.com>
2020-04-10 15:20:52 -0500

    ftp server

---
+++ ftp_server.py
@@ -0,0 +1,33 @@
+import os
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+def main():
+    # Instantiate a dummy authorizer for managing 'virtual' users
+    authorizer = DummyAuthorizer()
+
+    # Define a new user having full r/w permissions and a read-only
+    # anonymous user
+    os.chdir("/images")
+    authorizer.add_anonymous(os.getcwd())
+
+    # Instantiate FTP handler class
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    # Instantiate FTP server class and listen on 0.0.0.0:8000
+    address = ('', 8000)
+    server = FTPServer(address, handler)
+
+    # set a limit for connections
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    # start ftp server
+    server.serve_forever()
+
+    server.
+if __name__ == '__main__':
+    main()
